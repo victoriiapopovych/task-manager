134e6d2cf056451158979452c8080b5c83ab30f6
unknown <victoriiawww@gmail.com>
2026-03-17 22:33:41 +0200

    Add Task class

---
+++ src/tasks.py
@@ -0,0 +1,21 @@
+class Task :
+
+    def __init__(self, task_id, title, category="general", priority="medium", completed=False):
+        self.task_id = task_id
+        self.title = title
+        self.category = category
+        self.priority = priority
+        self.completed = completed
+
+    def show_info(self):
+        status = "Done" if self.completed else "Not done"
+        print(f"{self.task_id}. {self.title} \nStatus: [{status}] | Category: {self.category} | Priority: {self.priority}")
+
+    def to_dict(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "completed": self.completed,
+            "priority": self.priority,
+            "category": self.category
+        }
